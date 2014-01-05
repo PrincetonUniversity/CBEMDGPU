401c9a8915ec023f5b867bce2476e2bda112f44c
Carmeline J. Dsilva <cdsilva@adroit.Princeton.EDU>
2014-01-04 21:10:53 -0500

    Updated NVE files; added test for NVE

--- nve.cpp
+++ nve.cpp
@@ -1,5 +1,5 @@
 /*!
- * Do NVE integration
+ * Do NVE integration 
  * \author Nathan A. Mahynski
  * \date 11/18/13
  */
@@ -15,101 +15,83 @@
 
 /*!
  * Integrate a single timestep forward using Velocity-Verlet integration scheme.
- * Update the positions and velocities using velocity verlet integration.
- * This uses the accelerations stored on each atom.
  * Creates a cell list the first time it is called.
  * \param [in, out] sys System definition
- */  
+ */
 void nve::step (systemDefinition &sys) {
-	if (start_) {
-		try {
-			lastAccelerations_.resize(sys.numAtoms());
-		} catch (std::exception &e) {
-			std::cerr << e.what() << std::endl;
-			throw customException("Failed to initialize integrator due to memory constraints");
-			return;
-		}
-		try {
-			cellList_cpu tmpCL (sys.box(), sys.rcut(), sys.rskin());
-			cl_ = tmpCL;
-		} catch (std::exception &e) {
-			std::cerr << e.what() << std:: endl;
-			throw customException("Failed to integrate on first step");
-		}
+    int chunk = OMP_CHUNK;
+    if (start_) {
+        try {
+            cellList_cpu tmpCL (sys.box(), sys.rcut(), sys.rskin());
+            cl_ = tmpCL;
+        } catch (std::exception &e) {
+            std::cerr << e.what() << std:: endl;
+            throw customException("Failed to integrate on first step");
+        }
 
-		// calculate the forces initially (sets Up)
-		calcForce (sys);
-
-		// also get initial T
-		#pragma omp parallel
-		{
-#pragma omp for schedule(dynamic, OMP_CHUNK) 
-			for (unsigned int i = 0; i < sys.numAtoms(); ++i) {
-				sys.atoms[i].vel.x += dt_*0.5*(lastAccelerations_[i].x+sys.atoms[i].acc.x);
-				sys.atoms[i].vel.y += dt_*0.5*(lastAccelerations_[i].y+sys.atoms[i].acc.y);
-				sys.atoms[i].vel.z += dt_*0.5*(lastAccelerations_[i].z+sys.atoms[i].acc.z);
-			}
-		}
-		
-		float tmp = 0.0, Uk = 0.0;
-#pragma omp parallel
-		{
-#pragma omp for reduction(+:Uk) schedule(dynamic, OMP_CHUNK) 
-			for (unsigned int i = 0; i < sys.numAtoms(); ++i) {
-				Uk += (sys.atoms[i].vel.x*sys.atoms[i].vel.x)+(sys.atoms[i].vel.y*sys.atoms[i].vel.y)+(sys.atoms[i].vel.z*sys.atoms[i].vel.z);
-			}
-		}
-		Uk *= sys.mass();
-		tmp = Uk;
-		Uk *= 0.5;
-		tmp /= (3.0*(sys.numAtoms()-1.0));
-		sys.updateInstantTemp(tmp);
-		sys.setKinE(Uk);
-		start_ = 0;
-	}
-	
-	std::cout << "in nve" << std::endl;
-	// update positions based on current positions
-#pragma omp parallel
-	{
-#pragma omp for schedule(dynamic, OMP_CHUNK) 
-		for (unsigned int i = 0; i < sys.numAtoms(); ++i) {
-			sys.atoms[i].pos.x += dt_*(sys.atoms[i].vel.x+0.5*dt_*sys.atoms[i].acc.x);
-			sys.atoms[i].pos.y += dt_*(sys.atoms[i].vel.y+0.5*dt_*sys.atoms[i].acc.y);
-			sys.atoms[i].pos.z += dt_*(sys.atoms[i].vel.z+0.5*dt_*sys.atoms[i].acc.z);
-			lastAccelerations_[i] = sys.atoms[i].acc;
-		}
-	}
-	
-	// calculate new forces at new positions
-	calcForce (sys);
-	
-	// update velocities and get Uk and kinetic temperature
-#pragma omp parallel
-	{
-#pragma omp for schedule(dynamic, OMP_CHUNK) 
-		for (unsigned int i = 0; i < sys.numAtoms(); ++i) {
-			sys.atoms[i].vel.x += dt_*0.5*(lastAccelerations_[i].x+sys.atoms[i].acc.x);
-			sys.atoms[i].vel.y += dt_*0.5*(lastAccelerations_[i].y+sys.atoms[i].acc.y);
-			sys.atoms[i].vel.z += dt_*0.5*(lastAccelerations_[i].z+sys.atoms[i].acc.z);
-		}
-	}
+        // get initial temperature
+        calcForce(sys);
+        float tmp=0.0, Uk = 0.0;
+        #pragma omp parallel for reduction(+:Uk)
+        for (unsigned int i = 0; i < sys.numAtoms(); ++i) {
+            Uk += (sys.atoms[i].vel.x*sys.atoms[i].vel.x)+(sys.atoms[i].vel.y*sys.atoms[i].vel.y)+(sys.atoms[i].vel.z*sys.atoms[i].vel.z);
+        }
+        Uk *= sys.mass();
+        tmp = Uk;
+        Uk *= 0.5;
+        tmp /= (3.0*(sys.numAtoms()-1.0));
+        sys.updateInstantTemp(tmp);
+        sys.setKinE(Uk);
+        start_ = 0;
+    }
     
-	// get temperature and kinetic energy
-	float tmp = 0.0, Uk = 0.0;
-#pragma omp parallel
-	{
-#pragma omp for reduction(+:Uk) schedule(dynamic, OMP_CHUNK) 
-		for (unsigned int i = 0; i < sys.numAtoms(); ++i) {
-			Uk += (sys.atoms[i].vel.x*sys.atoms[i].vel.x)+(sys.atoms[i].vel.y*sys.atoms[i].vel.y)+(sys.atoms[i].vel.z*sys.atoms[i].vel.z);
-		}
-	}
-	Uk *= sys.mass();
-	tmp = Uk;
-	Uk *= 0.5;
-	tmp /= (3.0*(sys.numAtoms()-1.0));
-	sys.updateInstantTemp(tmp);
-	sys.setKinE(Uk);
-}
+    // (1) evolve particle velocities
+    #pragma omp parallel shared(sys)
+    {
+        #pragma omp for schedule(dynamic,OMP_CHUNK)
+        for (unsigned int i = 0; i < sys.numAtoms(); ++i) {
+            sys.atoms[i].vel.x += 0.5*dt_*(sys.atoms[i].acc.x);
+            sys.atoms[i].vel.y += 0.5*dt_*(sys.atoms[i].acc.y);
+            sys.atoms[i].vel.z += 0.5*dt_*(sys.atoms[i].acc.z);
+        }
 
+        // (2) evolve particle positions
+        #pragma omp for schedule(dynamic,OMP_CHUNK)
+        for (unsigned int i = 0; i < sys.numAtoms(); ++i) {
+            sys.atoms[i].pos.x += sys.atoms[i].vel.x*dt_;
+            sys.atoms[i].pos.y += sys.atoms[i].vel.y*dt_;
+            sys.atoms[i].pos.z += sys.atoms[i].vel.z*dt_;
+        }
+    }
+    
+    // (3) calc force
+    calcForce(sys);
+    
+    // (4) evolve particle velocities
+    #pragma omp parallel shared(sys)
+    {
+    #pragma omp for schedule(dynamic,OMP_CHUNK)
+    for (unsigned int i = 0; i < sys.numAtoms(); ++i) {
+            sys.atoms[i].vel.x += 0.5*dt_*(sys.atoms[i].acc.x);
+            sys.atoms[i].vel.y += 0.5*dt_*(sys.atoms[i].acc.y);
+            sys.atoms[i].vel.z += 0.5*dt_*(sys.atoms[i].acc.z);
+    }
+    }
+    float Uk = 0.0;
+    float tmp = 0.0;
+
+    #pragma omp parallel for reduction(+:Uk)
+    // get temperature and kinetic energy
+    for (unsigned int i = 0; i < sys.numAtoms(); ++i) {
+        Uk += (sys.atoms[i].vel.x*sys.atoms[i].vel.x)+(sys.atoms[i].vel.y*sys.atoms[i].vel.y)+(sys.atoms[i].vel.z*sys.atoms[i].vel.z);
+    }
+    
+    Uk *= sys.mass();
+    tmp = Uk;
+    Uk *= 0.5;
+    tmp /= (3.0*(sys.numAtoms()-1.0));
+    sys.updateInstantTemp(tmp);
+    sys.setKinE(Uk);
+
+}
 
